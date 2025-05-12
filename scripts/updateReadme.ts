442326aeed36cc1b504ef9b011303fdc41d9af85
kos5667 <kos5667@gmail.com>
2025-05-13 08:53:03 +0900

    path 수정.

--- scripts/updateReadme.ts
+++ scripts/updateReadme.ts
@@ -104,6 +104,6 @@ function buildProfileReadme() {
     contents += buildReadmeContents(newPost);
 
     postContents = postContents.filter(p => !newPost.some(post => post.filename === p.filename));
-    fs.writeFileSync(profileReadmePath, contents, 'utf-8');
+    fs.writeFileSync(path.join('..', 'dist', 'profile.md'), contents, 'utf-8');
 }
 buildProfileReadme();
